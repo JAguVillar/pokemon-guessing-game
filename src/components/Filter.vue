cc2d9ad8e4751d24ddf34ff86a9b8866c1cc0c70
JAguVillar <jaguvillar@gmail.com>
2024-01-09 16:31:04 -0300

    version base andando

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -1,79 +1,77 @@
 <template>
-    <div class="select-container mx-auto">
-        <input
-            class="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 py-2 px-4 bg-blue-50 text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent w-96"
-            placeholder="Your email"
-            type="text"
-            id="pokemon"
-            v-model="searchText"
-            @input="filterPokemon"
-            @focus="openDropdown"
-        />
-        <ul v-if="showDropdown" class="dropdown w-96">
-            <Item
-                @choose="log"
-                v-for="pokemon in filteredPokemon"
-                :key="pokemon"
-                :pokemon="pokemon.name"
-                :sprite="pokemon.sprite"
-                :type="pokemon.types"
-            />
-        </ul>
-    </div>
+  <div class="select-container mx-auto">
+    <input
+      class="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 py-2 px-4 bg-blue-50 text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent w-full"
+      placeholder="Your email"
+      type="text"
+      id="pokemon"
+      v-model="searchText"
+      @input="filterPokemon"
+      @focus="openDropdown"
+    />
+    <ul v-if="showDropdown" class="dropdown w-full">
+      <Item
+        @choose="log"
+        v-for="pokemon in filteredPokemon"
+        :key="pokemon"
+        :pokemon="pokemon.name"
+        :sprite="pokemon.sprite"
+        :type="pokemon.types"
+      />
+    </ul>
+  </div>
 </template>
 
 <script>
 import Item from "./Item.vue";
 
 export default {
-    components: { Item },
-    props: {
-        initialPokemonData: {
-            type: Array,
-            required: true,
-        },
+  components: { Item },
+  props: {
+    initialPokemonData: {
+      type: Array,
+      required: true,
     },
-    data() {
-        return {
-            searchText: "",
-            selectedPokemon: null,
-            allPokemon: null,
-            showDropdown: false,
-        };
+  },
+  data() {
+    return {
+      searchText: "",
+      selectedPokemon: null,
+      allPokemon: null,
+      showDropdown: false,
+    };
+  },
+  created() {
+    this.allPokemon = this.initialPokemonData;
+  },
+  computed: {
+    filteredPokemon() {
+      if (this.searchText.length >= 3) {
+        return this.allPokemon.filter((pokemon) =>
+          pokemon.name.toLowerCase().includes(this.searchText.toLowerCase())
+        );
+      }
     },
-    created() {
-        this.allPokemon = this.initialPokemonData;
+  },
+  methods: {
+    log(value) {
+      this.$emit("choose", value);
     },
-    computed: {
-        filteredPokemon() {
-            if (this.searchText.length) {
-                return this.allPokemon.filter((pokemon) =>
-                    pokemon.name
-                        .toLowerCase()
-                        .includes(this.searchText.toLowerCase())
-                );
-            }
-        },
+    filterPokemon() {
+      this.showDropdown = true;
+      // Add any additional filtering logic if needed
     },
-    methods: {
-        log(value) {
-            this.$emit("choose", value);
-        },
-        filterPokemon() {
-            this.showDropdown = true;
-            // Add any additional filtering logic if needed
-        },
-        openDropdown() {
-            this.showDropdown = true;
-        },
-        closeDropdown() {
-            this.showDropdown = false;
-        },
-        selectPokemon(pokemon) {
-            this.selectedPokemon = pokemon;
-            this.showDropdown = false;
-        },
+    openDropdown() {
+      this.showDropdown = true;
     },
+    closeDropdown() {
+      this.showDropdown = false;
+    },
+    selectPokemon(pokemon) {
+      this.selectedPokemon = pokemon;
+      this.showDropdown = false;
+    },
+  },
 };
 </script>
 
--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,78 +1,64 @@
 <template>
-    <div
-        class="container flex flex-col items-center justify-center w-full mx-auto bg-white rounded-lg shadow dark:bg-gray-800"
-    >
-        <ul class="flex flex-col divide-y divide">
-            <li class="flex flex-row" @click="log(pokemon, type)">
-                <div
-                    class="flex items-center flex-1 p-4 cursor-pointer select-none"
-                >
-                    <div
-                        class="flex flex-col items-center justify-center w-10 h-10 mr-4"
-                    >
-                        <img
-                            style="width: 120px"
-                            alt="profil"
-                            :src="sprite"
-                            class="mx-auto"
-                        />
-                    </div>
-                    <div class="flex-1 pl-1">
-                        <div
-                            class="fuente font-medium dark:text-white capitalize"
-                        >
-                            {{ pokemon }}
-                        </div>
-                    </div>
-                </div>
-            </li>
-        </ul>
+  <li
+    class="my-4 flex flex-row bg-slate-500 w-full rounded-xl"
+    @click="log(pokemon, type)"
+  >
+    <div class="flex items-center flex-1 cursor-pointer select-none">
+      <div class="flex flex-col items-center mr-4">
+        <img style="width: 120px" alt="profil" :src="sprite" class="mx-auto" />
+      </div>
+      <div class="flex-1 pl-1">
+        <div class="fuente font-medium capitalize">
+          {{ pokemon }}
+        </div>
+      </div>
     </div>
+  </li>
 </template>
 
 <script>
 export default {
-    props: {
-        pokemon: {
-            type: String,
-            required: true,
-        },
-        sprite: {
-            type: String,
-            required: true,
-        },
-        type: {
-            type: Array,
-            required: true,
-        },
+  props: {
+    pokemon: {
+      type: String,
+      required: true,
     },
-    data() {
-        return {
-            searchText: "",
-            selectedPokemon: null,
-            allPokemon: null,
-        };
+    sprite: {
+      type: String,
+      required: true,
     },
-    created() {
-        // this.allPokemon = this.initialPokemonData;
+    type: {
+      type: Array,
+      required: true,
     },
-    computed: {},
-    methods: {
-        log(p, t) {
-            this.$emit("choose", { pokemon: p, types: t });
-        },
+  },
+  data() {
+    return {
+      searchText: "",
+      selectedPokemon: null,
+      allPokemon: null,
+    };
+  },
+  created() {
+    // this.allPokemon = this.initialPokemonData;
+  },
+  computed: {},
+  methods: {
+    log(p, t) {
+      this.$emit("choose", { pokemon: p, types: t });
     },
+  },
 };
 </script>
 
 <style>
 /* Estilos CSS */
 @font-face {
-    font-family: "Pkmn";
-    src: url("/fonts/Pkmn.ttf") format("truetype");
+  font-family: "Pkmn";
+  src: url("/fonts/Pkmn.ttf") format("truetype");
 }
 
 * {
-    font-family: "Pkmn", sans-serif;
+  font-family: "Pkmn", sans-serif;
 }
 </style>
